5aaaa764cc93ccfb3f90b85a93faa81dd834a578
Alexander Langford <nirvashtzero@gmail.com>
2016-02-26 18:23:51 -0500

    Trying to figure out getopts stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate getopts;
+
+use getopts::Options;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args : Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "Print the help menu");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
+
+    if matches.opt_present("h") {
+        println!("You've asked for help!");
+    } else {
+        println!("You just want to run the program!");
+    }
 }
